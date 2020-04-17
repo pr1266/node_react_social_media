5b23691de163a1f1399e9fad6c8763089b7140d9
pr1266 <pourya_1999@yahoo.com>
2020-04-17 10:27:26 -0700

    'signup_validator'

--- node-basic/node-api/validators/index.js
+++ node-basic/node-api/validators/index.js
@@ -28,3 +28,28 @@ exports.createPostValidator = (req, res, next) => {
     }
     next();
 }
+
+
+exports.userSignupValidator = (req, res, next) => {
+    //! Name is not null and between 3 and 10 chars
+    req.check('name', 'Name is required').notEmpty();
+
+    //! check for email
+    //TODO not null and valid email
+
+    req.check('email', 'Email should not be null').matches(/.+\@.+\..+/).withMessage('enter a valid email').isLength({ min: 4, max: 2000});
+
+    //! check for password
+    req.check('password', 'password is required').isLength({ min: 6, max: 50});
+    //! check for errors
+    const errors = req.validationErrors();
+
+    if(errors){
+        const firstError = errors.map((error) => error.msg)[0];
+
+        return res.status(400).json({
+            error: firstError
+        });
+    }
+    next();
+}
