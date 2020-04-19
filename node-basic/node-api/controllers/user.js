cf5ec969fbfa3567c8a3fa6c06419f42e2f46b09
pr1266 <pourya_1999@yahoo.com>
2020-04-19 04:11:44 -0700

    ''update_users

--- node-basic/node-api/controllers/user.js
+++ node-basic/node-api/controllers/user.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const _ = require('lodash');
 
 exports.userById = (req, res, next, id) =>{
     User.findById(id).exec((err, user) =>{
@@ -40,4 +41,19 @@ exports.getUser = (req, res) =>{
     req.profile.hashed_password = undefined;
     req.profile.salt = undefined;
     return res.json(req.profile);
+}
+
+exports.updateUser = (req, res, next) => {
+    let user = req.profile;
+    user = _.extend(user, req.body);
+    user.updated = Date.now();
+    user.save((err) =>{
+        if(err){
+            return res.status(400).json({error: 'you are not authorized to perform this action'});
+        }
+        // user.profile.hashed_password = 0;
+        // user.profile.salt = undefined;
+
+        res.json({user})
+    });
 }
--- node-basic/node-api/routes/user.js
+++ node-basic/node-api/routes/user.js
@@ -2,11 +2,14 @@
 // const UserController = require('../controllers/auth');
 const express = require('express');
 const router = express.Router();
-const { allUsers, userById, getUser } = require('../controllers/user');
+const { allUsers, userById, getUser, updateUser } = require('../controllers/user');
 const {requireSignin} = require('../controllers/auth');
 
 router.get('/user', allUsers);
 router.get('/user/:userId', requireSignin, getUser);
+router.put('/user/:userId', requireSignin, updateUser);
+
+// router.get('/user/:userId/update', requireSignin, updateUser);
 //! any routes cotaining user id : our app execute user by id function
 router.param('userId', userById);
 
