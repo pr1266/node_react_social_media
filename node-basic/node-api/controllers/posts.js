b48782bde024722c95bbf1e5b7022256e5064cbb
pr1266 <pourya_1999@yahoo.com>
2020-04-20 12:00:51 -0700

    'createpostmethod_updated'

--- node-basic/node-api/controllers/posts.js
+++ node-basic/node-api/controllers/posts.js
@@ -1,6 +1,8 @@
-//controllers
+//! Controllers
 
 const Post = require('../models/post');
+const formidable = require('formidable');
+const fs = require('fs');
 
 const getPosts = (req, res) =>{
     const posts = Post.find().then(
@@ -13,11 +15,31 @@ const getPosts = (req, res) =>{
 }
 
 const createPost = (req, res) =>{
-    const post = new Post(req.body);
-    console.log('Creating Post ', req.body);
-    post.save((err, result) =>{
-        res.status(200).json({
-            post: result
+    
+    let form = new formidable.IncomingForm();
+    form.keepExtensions = true;
+    form.parse(req, (err, fields, files)=> {
+        //! check for errors:
+        if(err){
+            return res.status(400).json({
+                error: 'image could not be uploaded'
+            });
+        }
+
+        let post = new Post(fields);
+        post.user = req.profile;
+        if(files.photo){
+            post.photo.data = fs.readFileSync(files.photo.path);
+            post.photo.contentType = files.photo.type;
+        }
+
+        post.save((err, result) =>{
+            if(err){
+                return res.status(400).json({
+                    error: err
+                });
+            }
+            res.json(result);
         });
     });
 }
