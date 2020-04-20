78e35d838d9132cceb340aeb0f5bfa875eb2d2aa
pr1266 <pourya_1999@yahoo.com>
2020-04-20 15:58:32 -0700

    'docs'

--- node-basic/node-api/app.js
+++ node-basic/node-api/app.js
@@ -8,8 +8,9 @@ const dotenv = require('dotenv');
 const mongoClient = require('mongoose'), format = require('util').format;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const fs = require('fs');
 
-dotenv.config()
+dotenv.config();
 
 mongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true }).then(() => console.log('mongo db connected')).catch(() => console.log('fff'))
 
@@ -19,6 +20,18 @@ app.use(cookieParser());
 app.use('/', userRoutes);
 app.use('/', postRoutes);
 app.use('/', authRoutes);
+
+
+app.get('/', (req, res) =>{
+    fs.readFile('./docs/apiDocs.json', (err, data) =>{
+        if(err){
+            res.status(400).json({error: err});
+        }
+
+        const docs = JSON.parse(data);
+        res.json(docs);
+    });
+});
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`a node js server is running on port ${port}`));
--- node-basic/node-api/routes/post.js
+++ node-basic/node-api/routes/post.js
@@ -7,11 +7,12 @@ const validator = require('../validators/index');
 const UserController = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
-router.get('/', UserController.requireSignin, PostController.getPosts);
+router.get('/post', UserController.requireSignin, PostController.getPosts);
 router.post('/post/new/:userId', UserController.requireSignin, PostController.createPost, validator.createPostValidator);
 router.get('/post/:userId', UserController.requireSignin, PostController.postByUser);
 router.delete('/post/:postId', UserController.requireSignin, PostController.isPoster, PostController.deletePost);
 router.put('/post/:postId', UserController.requireSignin, PostController.isPoster, PostController.updatePost);
+
 router.param('userId', userById);
 router.param('postId', PostController.postById);
 
