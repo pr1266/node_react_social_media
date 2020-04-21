59a20165cb2efe08c13a43b705ce6cf40c9bbc8b
pr1266 <pourya_1999@yahoo.com>
2020-04-21 10:36:45 -0700

    'menu_modified_using_propb'

--- react-front/src/core/Menu.js
+++ react-front/src/core/Menu.js
@@ -1,21 +1,25 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, withRouter} from 'react-router-dom';
 
-const Menu = () => (
+const isActive = (history, path) => {
+    if(history.location.pathname === path) return {color: "#ff9900"};
+    else return {color: "#ffffff"};
+}
+
+const Menu = ({history}) => (
     <div>
-        
-<ul className='nav nav-tabs'>
-    <li className='nav-item'>
-        <Link className='nav-link' to='/'>Home</Link>
-    </li>
-    <li className='nav-item'>
-        <Link className='nav-link' to='/signin'>Sign in</Link>
-    </li>
-    <li className='nav-item'>
-        <Link className='nav-link' to='/signup'>Sign up</Link>
-    </li>
-</ul>
+        <ul className='nav nav-tabs bg-primary'>
+            <li className='nav-item'>
+                <Link className='nav-link' to='/' style={isActive(history, '/')}>Home</Link>
+            </li>
+            <li className='nav-item'>
+                <Link className='nav-link' to='/signin' style={isActive(history, '/signin')}>Sign in</Link>
+            </li>
+            <li className='nav-item'>
+                <Link className='nav-link' to='/signup' style={isActive(history, '/signup')}>Sign up</Link>
+            </li>
+        </ul>
     </div>
 );
 
-export default Menu;
+export default withRouter(Menu);
