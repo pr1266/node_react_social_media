b66308e2b01e176a86957e518979a862cd7b4851
pr1266 <pourya_1999@yahoo.com>
2020-04-21 12:21:01 -0700

    'signout'

--- react-front/src/core/Menu.js
+++ react-front/src/core/Menu.js
@@ -6,6 +6,21 @@ const isActive = (history, path) => {
     else return {color: "#ffffff"};
 }
 
+export const signout = (next) =>{
+    if(typeof window !== "undefined"){
+        localStorage.removeItem('token');
+        next();
+        fetch('http://localhost:8080/signout', {
+            method: 'GET'
+        })
+        .then(response => {
+            console.log('sign out ', response);
+            return response.json()
+        })
+        .catch(error => console.log(error));
+    }
+}
+
 const Menu = ({history}) => (
     <div>
         <ul className='nav nav-tabs bg-primary'>
@@ -17,6 +32,9 @@ const Menu = ({history}) => (
             </li>
             <li className='nav-item'>
                 <Link className='nav-link' to='/signup' style={isActive(history, '/signup')}>Sign up</Link>
+            </li>
+            <li className='nav-item'>
+                <a className='nav-link' onClick ={()=>signout(()=>history.push('/'))} style={isActive(history, '/'), {cursor: 'pointer', color: "#fff"}}>Signout</a>
             </li>
         </ul>
     </div>
