16b8c3380b333b6745774b9676ac1f86b5bb3373
pr1266 <pourya_1999@yahoo.com>
2020-04-20 17:23:51 -0700

    'signup-form'

--- react-front/src/user/Signup.js
+++ react-front/src/user/Signup.js
@@ -12,10 +12,44 @@ class Signup extends Component {
         }
     }
 
+    //! handle change of text inputs
+    handleChange = (_name) => (event) => {
+        console.log(this.state);
+        this.setState({
+            [_name]: event.target.value
+        });
+    }
+
+
     render(){
+        const {name, email, password} = this.state;
         return(
-            <div>
-                <h2>Sign up</h2>
+            <div className="container">
+                <h2 className="mt-5 mb-5">Sign up</h2>
+                
+                <form>
+                    <div className="form-group">
+                        <label className="text-muted">Name</label>
+                        <input onChange={this.handleChange("name")} type="text" className='form-control' value={name}/>
+                    </div>
+                    
+                    <div className="form-group">
+                        <label className="text-muted">Email</label>
+                        <input onChange={this.handleChange("email")} type="text" className='form-control' value={email}/>
+                    </div>
+                    
+                    <div className="form-group">
+                    <label className="text-muted">Password</label>
+                    <input onChange={this.handleChange("password")} type="password" className= 'form-control' value={password}/>
+                    </div>
+
+                    <div className="form-group">
+                    <button className="btn btn-raised btn-primary">
+                        Submit
+                    </button>
+                    </div>
+
+                </form>
             </div>
         );
     }
